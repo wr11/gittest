fc34c814f4e863ce74e2262117881fd55954709a
weihuawei <weihuawei@corp.netease.com>
2023-04-23 11:40:10 +0800

    定时提交

--- luatest/coroutime.lua
+++ luatest/coroutime.lua
@@ -8,19 +8,21 @@ co = coroutine.create(func)
 local c,d,e = coroutine.resume(co,1,2)
 print("coroutine_resume",c,d,e)
 coroutine.resume(co,5,6)
+print(coroutine.running(co))
+print(coroutine.resume(co,7,8))
 
 
-local function iterator()
-    for i=1,10 do
-        coroutine.yield(i)
-    end
-end
+-- local function iterator()
+--     for i=1,10 do
+--         coroutine.yield(i)
+--     end
+-- end
 
-co = coroutine.create(iterator)
-for i=1, math.huge do
-    local bSuc,ret = coroutine.resume(co)
-    print(bSuc,ret)
-    if coroutine.status(co) == "dead" then
-        break
-    end
-end
+-- co = coroutine.create(iterator)
+-- for i=1, 5 do
+--     local bSuc,ret = coroutine.resume(co)
+--     print(bSuc,ret)
+--     if coroutine.status(co) == "dead" then
+--         break
+--     end
+-- end
