91d38e355e3c4f4c7bf5bfda07746020a9e7079a
weihuawei <weihuawei@corp.netease.com>
2023-05-05 15:05:09 +0800

    定时提交

--- luatest/test.lua
+++ luatest/test.lua
@@ -1,8 +1,19 @@
-A = {}
-
-function a(t)
-    table.insert(t,1)
-    print(#t,#A)
+function test()
+    local o = {object=true}
+    print(o)
+    local weak = getWeakRef(o)
+    print(weak[1].object)
+    o = nil
+    collectgarbage()
+    print(weak[1].object)
 end
 
-a(A)
+function getWeakRef(o)
+    local tWeakRef = {}
+    local tMeta = {__mode = 'v'}
+    setmetatable(tWeakRef, tMeta)
+    table.insert(tWeakRef, o)
+    return tWeakRef
+end
+
+test()
